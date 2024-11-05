c725ca29124daaab2cdeeb16165f6a51d90605b1
teyeong <taetae0202@naver.com>
2024-11-05 19:35:53 +0900

    [03] JS basics

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var a = 12;
+var b = 34;
+
+var c = a + b;
+var d = a * b;
+
+console.log(c);
+console.log(d);
---
+++ test/test1.js
@@ -0,0 +1,7 @@
+var a = 12;
+var b = 34;
+console.log(a + b);
+
+var c = "abc";
+var d = "123";
+console.log(c + d);
